512fa6521dff00638a8b2f1e4b8ba649fd97c821
Héctor <hecmocer@gmail.com>
2016-04-04 12:38:49 +0200

    Funcionan peticiones GET, POST y DELETE de destinations
    comprobado con postman

--- Server/routes/api/v1/destinations.js
+++ Server/routes/api/v1/destinations.js
@@ -22,4 +22,34 @@ router.get('/', function(req, res, next) {
     });
 });
 
+// Petición POST
+router.post('/', function(req, res){
+    // Creamos un objeto con lo que nos pasen en el cuerpo de la petición
+    var destination = new Destination(req.body);
+
+    // Insertamos dicho objeto. Mongoose se encargará de comprobar si concuerdan los campos
+    destination.save(function(err, newRow){
+        if(err){
+            res.json( { result: false, error: err } );
+        }
+        else{
+            res.json( { result: true, insertedElement: newRow } );
+        }
+    })
+});
+
+// Petición DELETE
+router.delete('/:id', function(req, res){
+
+    // Borramos el elemento con el id especificado en la petición
+    Destination.remove({_id: req.params.id}, function(err, data){
+        if(err){
+            res.json( { result: false, error: err});
+        }
+        else{
+            res.json( { result: true, data: data});
+        }
+    });
+});
+
 module.exports = router;
