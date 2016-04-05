a8b7e37b6e0cdf90e0232f26de9d4440fc5ea062
Héctor <hecmocer@gmail.com>
2016-04-05 13:48:22 +0200

    Añadidas rutas

--- scripts/values/paths.js
+++ scripts/values/paths.js
@@ -1,5 +1,14 @@
 angular.module("babeladvisor").constant("paths", {
     root: "/",
     home: "/home",
+    users: "/users",
+    countryDetail: "/countries/:id",
+    countries: "/countries",
+    destinationDetail: "/destinations/:id",
+    destinations: "/destinations",
+    hotelDetail: "/hotels/:id",
+    hotels: "/hotels",
+    restaurantDetail: "/restaurants/:id",
+    restaurants: "/restaurants",
     error: "/error"
 });
