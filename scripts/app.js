2628cd201e94fcd46cc643005d6aaae2527e9eb8
Héctor <hecmocer@gmail.com>
2016-04-05 13:48:47 +0200

    Añadido ruteo de vistas con url

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,24 @@ angular.module("babeladvisor", ["ngRoute", "ngSanitize"]).config(
             redirectTo: paths.home
         }).when(paths.home, {
             templateUrl: "views/home.html"
+        }).when(paths.users, {
+            templateUrl: "views/users.html"
+        }).when(paths.countries, {
+            templateUrl: "views/countries.html"
+        }).when(paths.countryDetail, {
+            templateUrl: "views/countryDetail.html"
+        }).when(paths.destinations, {
+            templateUrl: "views/destinations.html"
+        }).when(paths.destinationDetail, {
+            templateUrl: "views/destinationDetail.html"
+        }).when(paths.hotels, {
+            templateUrl: "views/hotels.html"
+        }).when(paths.hotelDetail, {
+            templateUrl: "views/hotelDetail.html"
+        }).when(paths.restaurants, {
+            templateUrl: "views/restaurants.html"
+        }).when(paths.restaurantDetail, {
+            templateUrl: "views/restaurantDetail.html"
         }).when(paths.error, {
             templateUrl: "views/404.html"
         }).otherwise({
